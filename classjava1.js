dc19a751a2fa3de815e7afe1b8210e924aac5d73
khem <kchandool@yahoo.com>
2018-04-05 21:01:33 -0400

    Initial commit

---
+++ classjava1.js
@@ -0,0 +1,14 @@
+
+function Adder(){
+
+var x = document.getElementById("PriceVal").value;
+
+var OrigPrice = 399;
+
+var total1 = x * OrigPrice;
+
+ document.getElementById("total1").innerHTML = "USD " + total1 + ".00$";
+
+}
+
+
